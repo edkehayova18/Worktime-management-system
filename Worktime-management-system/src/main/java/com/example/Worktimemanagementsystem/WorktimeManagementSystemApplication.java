39e0bfd65ebca1a80c9454439c700e98781a318e
edkehayova18 <edkehayova18@codingburgas.bg>
2023-03-06 14:34:47 +0200

    Initial commit

---
+++ Worktime-management-system/src/main/java/com/example/Worktimemanagementsystem/WorktimeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Worktimemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktimeManagementSystemApplication.class, args);
+	}
+
+}
